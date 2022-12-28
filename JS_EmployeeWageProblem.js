c924607424b38c3622d6b13fcb35a320c1c9e902
Piyushnmj <piyushnmj@yahoo.com>
2022-12-28 18:57:55 +0530

    Check if employee is Present or Not

--- JS_EmployeeWageProblem.js
+++ JS_EmployeeWageProblem.js
@@ -1,1 +1,13 @@
 //Welcome to Employee Wage Problem
+
+//UC1
+const Is_Present = 0;
+let empCheck = Math.floor(Math.random() * 10 % 2);
+if(empCheck == Is_Present)
+{
+    console.log("Employee is Absent");
+}
+else
+{
+    console.log("Employee is Present");
+}
